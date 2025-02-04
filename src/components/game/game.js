883e3200ed33665b24eaab1036f8737c83925159
Alexander <test@tests-MacBook-Air.local>
2025-02-04 21:06:08 +0200

    fix change cards

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -10,19 +10,19 @@ const Game = () => {
     const [correct, setCorrect] = useState(0); //количество правильных ответов
 
     //текущий вопрос
-    const question = Questions[correct];
+    const question = Questions[step];
 
     const percent = Math.floor((step / Questions.length) * 100);
 
     const onClickVariant = (index) => {
-        setStep( step + 1 );
+        setStep(step + 1);
 
         if (index === question.correct) {
-            setCorrect( correct + 1 );
+            setCorrect(correct + 1);
         }
     }
 
-    //если все вопросы завершені показіваем результат
+    //если все вопросы завершены показываем результат
     if (step === Questions.length) {
         return  <Result correct={correct} />;
     }
@@ -33,7 +33,9 @@ const Game = () => {
             <div className="progress">
                 <div style={{ width: `${percent}%` }} className="progress__inner"></div>
             </div>
+
             <h1>{question.title}</h1>
+
             <ul>
                 {
                     question.variants.map((text, i) => {
--- src/components/result/result.js
+++ src/components/result/result.js
@@ -7,7 +7,9 @@ const  Result = ({correct}) => {
     return (
         <div className="result">
             <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+
             <h2>Вы отгадали {correct} ответа из {Questions.length}</h2>
+            
             <a href="/">
                 <button>Попробовать снова</button>
             </a>
